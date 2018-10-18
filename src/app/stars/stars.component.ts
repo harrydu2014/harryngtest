936b64d9404ff1a832a29036b596bdce5de43aa5
harrydu2014 <dumingyuan0829@163.com>
2018-10-18 08:32:27 +0800

    Oct 18 v1

--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -1,22 +1,36 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, SimpleChange, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-stars',
   templateUrl: './stars.component.html',
   styleUrls: ['./stars.component.css']
 })
-export class StarsComponent implements OnInit {
+export class StarsComponent implements OnInit, OnChanges {
   @Input()
   rating:number = 0;
   stars:boolean[];
-
+  
+  @Input()
+  readonly:boolean = true;
   constructor() { }
+  
+  @Output()
+  ratingChange: EventEmitter<number> =  new EventEmitter();
 
   ngOnInit() {
+  }
+
+  ngOnChanges(changes: SimpleChanges):void {
     this.stars = [];
     for(let i=1; i<=5; i++){
       this.stars.push(i > this.rating);
     }
   }
-
+  
+  clickStar(index:number){
+    if(!this.readonly){
+      this.rating = index + 1;
+      this.ratingChange.emit(this.rating);
+    }
+  }
 }
